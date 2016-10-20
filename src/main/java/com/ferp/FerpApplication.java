ce4297df6bb4c946d6d1892bcddff3aacdec21a7
lulus kana <lulus.kana@gmail.com>
2016-10-20 14:01:29 +0700

    update gitignore

---
+++ src/main/java/com/ferp/FerpApplication.java
@@ -0,0 +1,12 @@
+package com.ferp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerpApplication.class, args);
+	}
+}
